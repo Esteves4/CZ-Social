6021cbd708993649bb3466082071cce80169a3f8
Esteves4 <lucas.esteves.rocha@gmail.com>
2016-05-01 17:15:05 -0300

    Alterações de Pasta

--- send.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$to = "lucas.esteves.rocha@gmail.com";
-$subject = "Email de Confirmação"; 
-$message = "Clique no link para ativar sua conta."; 
-$mail_sent = @mail( $to, $subject, $message);
-echo $mail_sent ? "Mail sent" : "Mail failed";
-?>
-

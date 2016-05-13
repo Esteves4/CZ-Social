3d225fe1cf38913b15af8dae181171e29b673013
Lukiam23 <matheus.1596@hot.com>
2016-05-13 17:11:12 -0300

    Reajuste
    O mural por enquanto é 1, pois não achei uma forma de pegar o id do mural.

--- Controller/PostController-handler.php
+++ Controller/PostController-handler.php
@@ -11,8 +11,8 @@ if(isset($_POST['postar']))
 	$imagem = $_POST['imagem'];
 	$id_foto = $this->control->carregarFoto($imagem);
 	$texto = mysql_real_escape_string($_POST['texto']);
-  $mural = 1;
-  $this->control->publicar($mural,$texto, $id_foto);
+	$mural = 1;
+  	$this->control->publicar($mural,$texto, $id_foto);
 }
 
 ?>

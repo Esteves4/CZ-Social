15e96f39340b9ae0e00a45332d47e7c42ca1bb56
Lukiam23 <matheus.1596@hot.com>
2016-05-13 17:09:38 -0300

    Reajuste
    Adicionei um if

--- Controller/PostController-handler.php
+++ Controller/PostController-handler.php
@@ -3,10 +3,16 @@ require_once("CadastroController.php");
 
 $control = new PostController();
 
-$image = $_POST['imagem'];
 
-$id_foto = $this->control->carregarFoto($image);
 
-$texto = mysql_real_escape_string($_POST['texto']);
-$this->control->publicar($mural,$texto, $id_foto);
+
+if(isset($_POST['postar']))
+{
+	$imagem = $_POST['imagem'];
+	$id_foto = $this->control->carregarFoto($imagem);
+	$texto = mysql_real_escape_string($_POST['texto']);
+  $mural = 1;
+  $this->control->publicar($mural,$texto, $id_foto);
+}
+
 ?>
